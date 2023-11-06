6a5b9ae63d441aa62300d90e139f562fe02b571d
Denis Mbaya <117087528+mb4y4@users.noreply.github.com>
2023-11-06 19:43:46 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,21 +1,79 @@
 // Create web server application
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+var express = require('express');
+var app = express();
+var fs = require("fs");
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-// Create express application
-const app = express();
-
-// Add middleware
-app.use(bodyParser.json());
-app.use(cors());
-
-// Add route
-app.get('/', (req, res) => {
-  res.send('Hello from comments service');
+// Route: /list_user
+app.get('/list_user', function (req, res) {
+    fs.readFile(__dirname + "/" + "users.json", 'utf8', function (err, data) {
+        console.log(data);
+        res.end(data);
+    });
 });
 
-// Start application
-app.listen(4001, () => {
-  console.log('Listening on 4001');
+// Route: /add_user
+app.post('/add_user', urlencodedParser, function (req, res) {
+    // Prepare output in JSON format
+    response = {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        age: req.body.age,
+        email: req.body.email
+    };
+    console.log(response);
+    // Write output to response
+    fs.readFile(__dirname + "/" + "users.json", 'utf8', function (err, data) {
+        data = JSON.parse(data);
+        data["user4"] = response;
+        console.log(data);
+        res.end(JSON.stringify(data));
+    });
 });
+
+// Route: /:id
+app.get('/:id', function (req, res) {
+    // First read existing users.
+    fs.readFile(__dirname + "/" + "users.json", 'utf8', function (err, data) {
+        // Parse data
+        users = JSON.parse(data);
+        // Get user by id
+        var user = users["user" + req.params.id];
+        console.log(user);
+        // Write output to response
+        res.end(JSON.stringify(user));
+    });
+});
+
+// Route: /delete_user
+app.delete('/delete_user', urlencodedParser, function (req, res) {
+    // First read existing users.
+    fs.readFile(__dirname + "/" + "users.json", 'utf8', function (err, data) {
+        // Parse data
+        data = JSON.parse(data);
+        // Delete user
+        delete data["user" + req.body.id];
+        console.log(data);
+        // Write output to response
+        res.end(JSON.stringify(data));
+    });
+});
+
+// Path: /index.htm
+app.get('/index.htm', function (req, res) {
+    res.sendFile(__dirname + "/" + "index.htm");
+});
+
+// Path: /process_get
+app.get('/process_get', function (req, res) {
+    // Prepare output in JSON format
+    response = {
+        first_name: req.query.first_name,
+        last_name: req.query.last_name
+    };
+    console.log(response);
+    // Write output to response
+    res.end(JSON.stringify(response));
+}
+);
